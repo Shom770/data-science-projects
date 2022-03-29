b8112850a3a778ef54bf742e091df6bec0785632
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-03-28 21:51:35 -0400

    another foundation

--- winter_storm_jonas_hourly.py
+++ winter_storm_jonas_hourly.py
@@ -18,9 +18,34 @@ ax.add_feature(USCOUNTIES.with_scale("5m"))
 
 session = requests.session()
 
-stations_json = session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&network=MD_COOP").json()
+stations_json = session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-22&network=MD_COOP").json()
+stations_json["features"].extend(
+    session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-22&network=VA_COOP")
+    .json()["features"]
+)
+stations_json["features"].extend(
+    session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-22&network=WV_COOP")
+    .json()["features"]
+)
+
+
+# stations_d2_json = session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-24&network=MD_COOP").json()
+# stations_d2_json["features"].extend(
+#     session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-24&network=VA_COOP")
+#     .json()["features"]
+# )
+# stations_d2_json["features"].extend(
+#     session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-24&network=WV_COOP")
+#     .json()["features"]
+# )
+
 for station in stations_json["features"]:
-    if station["properties"]["snow"]:
-        print(station["properties"]["name"], "=", station["properties"]["snow"], "\"", sep="")
+    long, lat = station["geometry"]["coordinates"]
+    if (
+            station["properties"]["snow"]
+            and extent[0] + 0.02 <= long <= extent[1] - 0.02
+            and extent[2] + 0.02 <= lat <= extent[3] - 0.02
+    ):
+        ax.text(long, lat, round(station["properties"]["snow"]))
 
 plt.show()
