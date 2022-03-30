e685229eb756d4134cba3b8b5ad0eabf9b5ce665
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-03-30 19:51:26 -0400

    not with logging

--- experimenting/reading_nam_model_data.py
+++ experimenting/reading_nam_model_data.py
@@ -21,32 +21,56 @@ ax.add_feature(cfeature.OCEAN.with_scale("10m"))
 ax.add_feature(USCOUNTIES.with_scale("5m"))
 
 
-def chaikins_corner_cutting(coords, refinements=5):
-    coords = np.array(coords)
-
-    for _ in range(refinements):
-        L = coords.repeat(2, axis=0)
-        R = np.empty_like(L)
-        R[0] = L[0]
-        R[2::2] = L[1:-1:2]
-        R[1:-1:2] = L[2::2]
-        R[-1] = L[-1]
-        coords = L * 0.75 + R * 0.25
-
-    return coords
-
-
 def filter_cape(cape_coords):
+    filt_cape = [[]]
+    for latitude in {tup[-1] for tup in cape_coords}:
+        all_pts = sorted([coord for coord in cape_coords if round(coord[-1], 2) == round(latitude, 2)])
+        if all_pts[0] == all_pts[-1]:
+            filt_cape[-1].append(all_pts[0])
+            filt_cape.append([])
+        else:
+            filt_cape[-1].append(all_pts[0])
+            filt_cape[-1].append(all_pts[-1])
 
-data = Dataset(
+    filt_cape = [sorted(lst, key=itemgetter(1)) for lst in filt_cape]
+    results = []
+
+    for poly in filt_cape:
+        lons = [tup[0] for tup in poly]
+        lats = [tup[1] for tup in poly]
+
+        if len(lons[::2] + lons[-1::-2]) > 4 and len(lats[::2] + lats[-1::-2]) > 4:
+            results.extend([
+                lons[::2] + lons[-1::-2],
+                lats[::2] + lats[-1::-2],
+            ])
+
+    return results
+
+
+data_nam = Dataset(
     (
         f"http://nomads.ncep.noaa.gov/dods/nam/"
         f"nam{datetime.now().strftime('%Y%m%d')}/nam_18z"
     )
 )
+data_nam3k = Dataset(
+    (
+        f"http://nomads.ncep.noaa.gov/dods/nam/"
+        f"nam{datetime.now().strftime('%Y%m%d')}/nam_conusnest_18z"
+    )
+)
+data_hrrr = Dataset(
+    (
+        f"http://nomads.ncep.noaa.gov/dods/hrrr/"
+        f"hrrr{datetime.now().strftime('%Y%m%d')}/hrrr_sfc.t18z"
+    )
+)
 
-lons = data.variables["lon"][:]
-lats = data.variables["lat"][:]
+lons = data_nam3k.variables["lon"][:]
+lats = data_nam3k.variables["lat"][:]
+lons_n = data_nam.variables["lon"][:]
+lats_n = data_nam.variables["lat"][:]
 
 home_lat = np.where(
     np.logical_and(np.greater_equal(lats, 37), np.less_equal(lats, 40))
@@ -54,32 +78,55 @@ home_lat = np.where(
 home_lon = np.where(
     np.logical_and(np.greater_equal(lons, -80), np.less_equal(lons, -74))
 )[0]
+home_lat_nam = np.where(
+    np.logical_and(np.greater_equal(lats_n, 37), np.less_equal(lats_n, 40))
+)[0]
+home_lon_nam = np.where(
+    np.logical_and(np.greater_equal(lons_n, -80), np.less_equal(lons_n, -74))
+)[0]
 
-cape0_poly = []
+cape500_poly_nam3k = []
+cape500_poly_nam = []
+cape500_poly_hrrr = []
 
-cape_cur_hour = data.variables["capesfc"][33 / 3]
+cape_nam = data_nam.variables["capesfc"][27 / 3]
+cape_nam3k = data_nam3k.variables["capesfc"][27 / 3]
+cape_hrrr = data_hrrr.variables["capesfc"][27]
+
+print("starting")
 
 for lat in home_lat:
     for lon in home_lon:
-        if cape_cur_hour[lat, lon] > 500.0:
-            cape0_poly.append((lons[lon], lats[lat]))
+        if cape_nam3k[lat, lon] > 500:
+            cape500_poly_nam3k.append((lons[lon], lats[lat]))
+        if cape_hrrr[lat, lon] > 500:
+            cape500_poly_hrrr.append((lons[lon], lats[lat]))
 
-filt_cape0 = []
-for latitude in {tup[-1] for tup in cape0_poly}:
-    all_pts = sorted([coord for coord in cape0_poly if round(coord[-1], 2) == round(latitude, 2)])
-    filt_cape0.append(all_pts[0])
-    filt_cape0.append(all_pts[-1])
+print("done with nam3k/HRRR")
 
-filt_cape0 = sorted(filt_cape0, key=itemgetter(1))
+for lat in home_lat_nam:
+    for lon in home_lon_nam:
+        if cape_nam[lat, lon] > 500:
+            cape500_poly_nam.append((lons_n[lon], lats_n[lat]))
 
-lons = [tup[0] for tup in filt_cape0]
-lats = [tup[1] for tup in filt_cape0]
+print("done with NAM")
 
-ax.fill(
-    chaikins_corner_cutting(lons[::2] + lons[-1::-2], refinements=7),
-    chaikins_corner_cutting(lats[::2] + lats[-1::-2], refinements=7),
-    alpha=0.5, facecolor="blue", transform=ccrs.PlateCarree()
-)
+if cape500_poly_nam3k:
+    ax.plot(
+        *filter_cape(cape500_poly_nam3k),
+        alpha=0.5, c="red", lw=5, label="NAM NEST", transform=ccrs.PlateCarree()
+    )
+if cape500_poly_hrrr:
+    ax.plot(
+        *filter_cape(cape500_poly_hrrr),
+        alpha=0.5, c="green", lw=5, label="HRRR", transform=ccrs.PlateCarree()
+    )
+if cape500_poly_nam:
+    ax.plot(
+        *filter_cape(cape500_poly_nam),
+        alpha=0.5, c="blue", lw=5, label="NAM", transform=ccrs.PlateCarree()
+    )
 
+ax.legend()
 
 plt.show()
