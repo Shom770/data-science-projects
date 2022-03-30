83903b14e73eb9ff25cc32153e5f7f2417654d10
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-03-30 13:17:48 -0400

    first prototype

--- winter_storm_jonas_hourly.py
+++ winter_storm_jonas_hourly.py
@@ -1,79 +1,131 @@
 import json
-from datetime import datetime
+
+from datetime import datetime, timedelta
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import requests
 
-from metpy.plots import USCOUNTIES
+from matplotlib.animation import FuncAnimation
+from pandas import date_range
 
-# extent = (-79.602563, -75.723267, 37.035112, 39.724089)
-#
-# fig: plt.Figure = plt.figure()
-# ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-#
-# ax.set_extent(extent)
-#
-# ax.add_feature(cfeature.LAND.with_scale("10m"))
-# ax.add_feature(cfeature.OCEAN.with_scale("10m"))
-# ax.add_feature(USCOUNTIES.with_scale("5m"))
+extent = (-79.602563, -75.723267, 37.035112, 40)
+text_lon = sum(extent[:2]) / 2
+text_lat = (40 + 39.6) / 2
+
+fig: plt.Figure = plt.figure()
+ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
+ax.set_extent(extent)
+
+ax.add_feature(cfeature.LAND.with_scale("10m"))
+ax.add_feature(cfeature.OCEAN.with_scale("10m"))
+ax.add_feature(cfeature.STATES.with_scale("10m"))
 
 session = requests.session()
+lines = []
 
-to_write = {}
+with open("storm_totals.json", "r") as totals_file:
+    storm_totals = json.loads(totals_file.read())
 
-iad_snow, = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&station=IAD&network=VA_ASOS"
-).json()["features"]
 
-iad_hourlyp = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/obhistory.json?date=2016-01-23&station=IAD&network=VA_ASOS"
-).json()
+def snow_color_table(total_snow):
+    if total_snow == 0:
+        return "#ffffff"
+    elif 0.1 <= total_snow < 1:
+        return "#bdd8e6"
+    elif 1 <= total_snow < 2:
+        return "#6bb0d6"
+    elif 2 <= total_snow < 3:
+        return "#3284bf"
+    elif 3 <= total_snow < 4:
+        return "#07519d"
+    elif 4 <= total_snow < 6:
+        return "#082695"
+    elif 6 <= total_snow < 8:
+        return "#ffff97"
+    elif 8 <= total_snow < 12:
+        return "#fdc400"
+    elif 12 <= total_snow < 18:
+        return "#ff8801"
+    elif 18 <= total_snow < 24:
+        return "#db1300"
+    elif 24 <= total_snow < 30:
+        return "#9f0002"
+    elif 30 <= total_snow < 36:
+        return "#690001"
+    elif total_snow >= 36:
+        return "#330101"
 
-dca_snow, = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&station=DCA&network=VA_ASOS"
-).json()["features"]
 
-dca_hourlyp = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/obhistory.json?date=2016-01-23&station=DCA&network=VA_ASOS"
-).json()
+def size_table(total_snow):
+    if total_snow == 0:
+        return 40
+    elif 0.1 <= total_snow < 1:
+        return 50
+    elif 1 <= total_snow < 2:
+        return 60
+    elif 2 <= total_snow < 3:
+        return 70
+    elif 3 <= total_snow < 4:
+        return 80
+    elif 4 <= total_snow < 6:
+        return 100
+    elif 6 <= total_snow < 8:
+        return 120
+    elif 8 <= total_snow < 12:
+        return 170
+    elif 12 <= total_snow < 18:
+        return 225
+    elif 18 <= total_snow < 24:
+        return 275
+    elif 24 <= total_snow < 30:
+        return 350
+    elif 30 <= total_snow < 36:
+        return 425
+    elif total_snow >= 36:
+        return 500
 
-bwi_snow, = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&station=BWI&network=MD_ASOS"
-).json()["features"]
 
-bwi_hourlyp = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/obhistory.json?date=2016-01-23&station=BWI&network=MD_ASOS"
-).json()
+def animation_frames():
+    yield from date_range(datetime(2016, 1, 22, 6), periods=48, freq="H").tolist()[1:]
 
-lyh_snow, = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&station=LYH&network=VA_ASOS"
-).json()["features"]
 
-lyh_hourlyp = session.get(
-    "https://mesonet.agron.iastate.edu/api/1/obhistory.json?date=2016-01-23&station=LYH&network=VA_ASOS"
-).json()
+def animate(frame):
+    for artist in lines:
+        artist.set_visible(False)
 
-for station in (iad_snow, bwi_snow, dca_snow, lyh_snow):
-    to_write[station["properties"]["id"]] = {
-        "name": station["properties"]["name"],
-        "snow": station["properties"]["snow"],
-        "coordinates": station["geometry"]["coordinates"]
-    }
+    current_time = datetime.fromisoformat(str(frame).replace(" ", "T")) - timedelta(hours=5)
 
-for id_, hourly_station in zip(("IAD", "BWI", "DCA", "LYH"), (iad_hourlyp, bwi_hourlyp, dca_hourlyp, lyh_hourlyp)):
-    to_write[id_]["hourly_precip"] = []
-    for dp in hourly_station["data"]:
-        if datetime.fromisoformat(dp["local_valid"]).minute >= 50:
-            to_write[id_]["hourly_precip"].append(dp["p01i"])
+    for station, observations in storm_totals.items():
+        total_snow = round(sum(
+                observations["hourly_snow"][:current_time.hour if current_time.day == 22 else 24 + current_time.hour]
+        ),  1)
 
-    ratio = to_write[id_]["snow"] / sum([(precip * 10) for precip in to_write[id_]["hourly_precip"]])
+        lines.append(ax.scatter(
+            *observations["coordinates"],
+            c=snow_color_table(total_snow),
+            edgecolor="black",
+            s=size_table(total_snow),
+            transform=ccrs.PlateCarree()
+        ))
 
-    to_write[id_]["hourly_snow"] = [precip * 10 * ratio for precip in to_write[id_]["hourly_precip"]]
+    lines.append(ax.text(
+        text_lon,
+        text_lat,
+        current_time.strftime("%B %d, %Y at %I:%M %p"),
+        size=14,
+        horizontalalignment="center",
+        bbox={'facecolor': '#c0c0c0', 'edgecolor': 'black', 'boxstyle': 'round'}
+    ))
 
-with open("jan_23.json", "w") as file:
-    file.write(json.dumps(to_write))
+    return lines
+
+
+anim = FuncAnimation(fig, animate, frames=animation_frames, interval=20, blit=False)
+
+anim.save("./progress.gif")
 
 # stations_json = session.get("https://mesonet.agron.iastate.edu/api/1/daily.geojson?date=2016-01-23&network=MD_COOP").json()
 # stations_json["features"].extend(
