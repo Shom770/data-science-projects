8179d860f44508403192e2d6247c0b6cbe0b67ec
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-04-12 13:28:51 -0400

    fix bugs

--- 2016_blizzard_hourly_loop/winter_storm_jonas_hourly.py
+++ 2016_blizzard_hourly_loop/winter_storm_jonas_hourly.py
@@ -1,6 +1,7 @@
 import json
 
 from datetime import datetime, timedelta
+from operator import itemgetter
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -30,17 +31,18 @@ for font in font_manager.findSystemFonts(["."]):
 # Set font family globally
 matplotlib.rcParams['font.family'] = 'Inter'
 
+POINTS_BETWEEN = 5
+
 session = requests.session()
 lines = []
+visited = set()
 
 with open("storm_totals.json", "r") as totals_file:
     storm_totals = json.loads(totals_file.read())
 
 
 def snow_color_table(total_snow):
-    if total_snow == 0:
-        return "#ffffff"
-    elif 0.1 <= total_snow < 1:
+    if 0.1 <= total_snow < 1:
         return "#bdd8e6"
     elif 1 <= total_snow < 2:
         return "#6bb0d6"
@@ -64,6 +66,23 @@ def snow_color_table(total_snow):
         return "#690001"
     elif total_snow >= 36:
         return "#330101"
+
+    return "#ffffff"
+
+
+def distance(c1, c2):
+    return (abs(c1[0] - c2[0]) ** 2 + abs(c1[1] - c2[1]) ** 2) ** 0.5
+
+
+def adjacent_stations(coords, stn_name):
+    res = sorted(
+        [
+            (name, distance(stn_data["coordinates"], coords), tuple(stn_data["coordinates"]))
+            for name, stn_data in {"DALV2": storm_totals["DALV2"], "DMAM2": storm_totals["DMAM2"]}.items()
+            if name != stn_name
+        ], key=itemgetter(1)
+    )
+    return res[:5]
 
 
 def size_table(total_snow):
@@ -100,6 +119,9 @@ def animation_frames():
 
 
 def animate(frame):
+    global visited
+    visited = set()
+
     for artist in lines[:]:
         artist.set_visible(False)
         del artist
@@ -107,7 +129,7 @@ def animate(frame):
     current_time = datetime.fromisoformat(str(frame).replace(" ", "T")) - timedelta(hours=5)
 
     all_obs = []
-    for station, observations in storm_totals.items():
+    for station, observations in {"DALV2": storm_totals["DALV2"], "DMAM2": storm_totals["DMAM2"]}.items():
         total_snow = round(sum(
                 observations["hourly_snow"][:current_time.hour if current_time.day == 22 else 24 + current_time.hour]
         ),  1)
@@ -121,6 +143,55 @@ def animate(frame):
             transform=ccrs.PlateCarree()
         ))
 
+        for closest in adjacent_stations(observations["coordinates"], station):
+            if closest[0] in visited:
+                continue
+            else:
+                cc = closest[-1]  # The coordinates for one of the five closest stations.
+                dist_lon = abs(cc[0] - observations["coordinates"][0])
+                min_lon = min((cc[0], observations["coordinates"][0]))
+
+                dist_lat = abs(cc[1] - observations["coordinates"][1])
+                min_lat = min((cc[1], observations["coordinates"][1]))
+
+                stn_snow = round(sum(
+                    storm_totals[closest[0]][
+                        "hourly_snow"
+                    ][:current_time.hour if current_time.day == 22 else 24 + current_time.hour]
+                ), 1)
+                diff_snow = abs(stn_snow - total_snow)
+                min_snow, stn_min = min(((stn_snow, closest[0]), (total_snow, station)), key=itemgetter(0))
+
+                between_coords = sorted([
+                    (
+                        min_lon + dist_lon * (num / POINTS_BETWEEN), min_lat + dist_lat * (num / POINTS_BETWEEN)
+                    ) for num in range(1, POINTS_BETWEEN)
+                ], key=lambda coord: distance(coord, storm_totals[stn_min]["coordinates"]))
+
+                for coord_num, bet_coord in enumerate(between_coords, start=1):
+                    pt_snow = min_snow + diff_snow * (coord_num / POINTS_BETWEEN)
+                    lines.append(ax.scatter(
+                        *bet_coord,
+                        c=snow_color_table(pt_snow),
+                        edgecolor="black",
+                        s=size_table(pt_snow),
+                        transform=ccrs.PlateCarree()
+                    ))
+
+
+                # for num in range(*range_args):
+                #     coords = [min_lon + dist_lon * (num / 5), min_lat + dist_lat * (num / 5)]
+                #     pt_snow = min_snow + diff_snow * (num / 5)
+                #     lines.append(ax.scatter(
+                #         *coords,
+                #         c=snow_color_table(pt_snow),
+                #         edgecolor="black",
+                #         s=size_table(pt_snow),
+                #         transform=ccrs.PlateCarree()
+                #     ))
+
+        visited.add(station)
+
     cur_time = AnchoredText(
         current_time.strftime("%B %d, %Y at %I:%M %p"),
         loc='upper center',
@@ -133,6 +204,6 @@ def animate(frame):
     return lines
 
 
-anim = FuncAnimation(fig, animate, frames=animation_frames, blit=False)
+anim = FuncAnimation(fig, animate, frames=animation_frames, interval=500, blit=False)
 
-anim.save("./progress.gif")
+plt.show()
