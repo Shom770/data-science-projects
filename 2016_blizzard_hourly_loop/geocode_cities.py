f9c15a2f0dccd43874159a01799a8159fef6ed18
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-04-18 12:50:33 -0400

    misspelling

--- 2016_blizzard_hourly_loop/geocode_cities.py
+++ 2016_blizzard_hourly_loop/geocode_cities.py
@@ -15,15 +15,15 @@ list_of_cities = [
     "Lexington Park, MD",
     "Martinsburg, WV",
     "Winchester, VA",
-    "Culpeper, MD"
+    "Culpeper, VA"
 ]
 loc = {}
 
 for city in list_of_cities:
-    loc[city.split(" ")[0].removesuffix(",")] = (
+    loc[city.split(",")[0]] = (
         session.get(
             f"https://nominatim.openstreetmap.org/search?q={city}&format=geojson"
-        ).json()["features"][0]["geometry"]["coordinates"]
+        ).json()["features"]
     )
 
 with open("cities.json", "w") as file:
