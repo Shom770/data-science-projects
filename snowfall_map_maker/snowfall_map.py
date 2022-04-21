35f35e14366dc954f1e7f44a5b2ced847f7c3afa
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-04-21 18:19:58 -0400

    finish report getting feature

--- snowfall_map_maker/snowfall_map.py
+++ snowfall_map_maker/snowfall_map.py
@@ -6,10 +6,20 @@ import requests
 ISO8660 = "%Y-%m-%dT%H:%M"
 URL = "http://mesonet.agron.iastate.edu/cgi-bin/request/gis/lsr.py"
 
-
-def get_reports(start_time: datetime.datetime, end_time: datetime.datetime) -> None:
-    request = requests.get(f"{URL}?state=MD&sts={start_time.strftime(ISO8660)}&ets={end_time.strftime(ISO8660)}")
-    print(request.text)
+session = requests.session()
 
 
-get_reports(datetime.datetime(2022, 1, 7), datetime.datetime(2022, 1, 8))
+def get_reports(states: list, start_time: datetime.datetime, end_time: datetime.datetime) -> None:
+    totals = {}
+    for state in states:
+        req = session.get(
+                f"{URL}?state={state}&sts={start_time.strftime(ISO8660)}&ets={end_time.strftime(ISO8660)}&justcsv=true"
+        ).text
+        for line in req.split("\n"):
+            split_line = line.split(",")
+            totals[tuple(map(float, split_line[2:4]))] = float(split_line[5])
+
+    return totals
+
+
+print(get_reports(["MD", "VA"], datetime.datetime(2022, 1, 7), datetime.datetime(2022, 1, 8)))
