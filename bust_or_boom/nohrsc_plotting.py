210ef353588467377ef4a9c259b9d5bcf98c976b
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-04-30 15:59:20 -0400

    finish NOHRSC plot!!

--- bust_or_boom/nohrsc_plotting.py
+++ bust_or_boom/nohrsc_plotting.py
@@ -1,22 +1,27 @@
 import datetime
-import os
 import urllib.request
+import os
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.colors as colors
 import xarray
 
-DATA_TIME = datetime.datetime(year=2022, month=1, day=8)
+# Remove any .nc files that already exist
+directory = next(os.walk("./"))
+for file_name in directory[-1]:
+    if file_name.endswith(".nc"):
+        os.remove(file_name)
+
+DATA_TIME = datetime.datetime(year=2022, month=1, day=17)
 FILE_PATH = f"{DATA_TIME.strftime('%Y%m%d%H')}.nc"
 URL = (
         f"http://www.nohrsc.noaa.gov/snowfall_v2/data/"
         f"{DATA_TIME.strftime('%Y%m')}/sfav2_CONUS_24h_{DATA_TIME.strftime('%Y%m%d%H')}.nc"
     )
 
-if not os.path.exists(f"{DATA_TIME.strftime('%Y%m%d%H')}.nc"):
-    urllib.request.urlretrieve(URL, FILE_PATH)
+urllib.request.urlretrieve(URL, FILE_PATH)
 
 dataset = xarray.open_mfdataset(FILE_PATH)
 
@@ -36,7 +41,13 @@ snow_data = dataset.Data * 39
 lats = snow_data.lat.values
 lons = snow_data.lon.values
 
+levels = [0.1, 1, 2, 3, 4, 6, 8, 12, 16, 20, 24, 30, 36, 48, 60, 72]
+cmap = colors.ListedColormap([
+    '#bdd7e7', '#6baed6', '#3182bd', '#08519c', '#082694', '#ffff96',
+    '#ffc400', '#ff8700', '#db1400', '#9e0000', '#690000', '#ccccff',
+    '#9f8cd8', '#7c52a5', '#561c72', '#40dfff'
+])
+norm = colors.BoundaryNorm(levels, cmap.N)
 
-
-ax.contourf(lons, lats, snow_data.values, alpha=0.5)
+ax.contourf(lons, lats, snow_data.values, levels, alpha=0.5, cmap=cmap, norm=norm, transform=ccrs.PlateCarree())
 plt.show()
