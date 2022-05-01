ef4b2eb806b1b4002a1d0809b3ab41c21846f049
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-01 17:05:59 -0400

    what i have so far

--- bust_or_boom/historical_hrrr.py
+++ bust_or_boom/historical_hrrr.py
@@ -1,24 +1,37 @@
 import os
+from datetime import datetime, timedelta
 
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
 import xarray
 
-# Remove any .grib2 files that already exist
-directory = next(os.walk("./"))
-for file_name in directory[-1]:
-    if file_name.endswith(".grib2") or file_name.endswith(".idx"):
-        os.remove(file_name)
 
+def historical_hrrr_snow(extent, type_="snowfall"):
+    data_time = datetime(year=2022, month=1, day=8)
 
-def historical_hrrr_snow(data_time):
+    if type_ == "depth":
+        hours = 0
+        step_type = "instant"
+        short_name = "sde"
+    else:
+        hours = 24
+        step_type = "accum"
+        short_name = "asnow"
+
+    min_lon, max_lon, min_lat, max_lat = extent
+    data_time = data_time - timedelta(hours=hours)
     BUCKET_NAME = 'noaa-hrrr-bdp-pds'
-    S3_OBJECT = f"hrrr.{data_time.strftime('%Y%m%d')}/conus/hrrr.t00z.wrfsfcf24.grib2"
+    S3_OBJECT = f"hrrr.{data_time.strftime('%Y%m%d')}/conus/hrrr.t00z.wrfsfcf{str(hours).zfill(2)}.grib2"
 
     FILE_PATH = S3_OBJECT.split("/")[-1]
 
     if not os.path.exists(FILE_PATH):
+        directory = next(os.walk("./"))
+        for file_name in directory[-1]:
+            if file_name.endswith(".grib2") or file_name.endswith(".idx"):
+                os.remove(file_name)
+
         s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
         obj = s3.get_object(Bucket=BUCKET_NAME, Key=S3_OBJECT)['Body'].read()
 
@@ -28,9 +41,15 @@ def historical_hrrr_snow(data_time):
     dataset = xarray.open_dataset(
         FILE_PATH,
         engine="cfgrib",
-        filter_by_keys={'stepType': 'accum', 'typeOfLevel': 'surface'}
+        filter_by_keys={'stepType': step_type, 'typeOfLevel': 'surface'},
+        decode_times=False
     )
+
+    mask_lon = (dataset.longitude - 359.99 >= min_lon) & (dataset.longitude - 360 <= max_lon)
+    mask_lat = (dataset.latitude >= min_lat) & (dataset.latitude <= max_lat)
+
+    dataset = dataset.where(mask_lon & mask_lat, drop=True)
     lons = dataset["longitude"].values
     lats = dataset["latitude"].values
 
-    return lons - 359.99, lats, dataset["asnow"].values * 39.3700787
+    return lons - 359.99, lats, dataset[short_name].values * 39.3700787
--- bust_or_boom/nohrsc_plotting.py
+++ bust_or_boom/nohrsc_plotting.py
@@ -2,30 +2,35 @@ import datetime
 import urllib.request
 import os
 
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import matplotlib.pyplot as plt
 import xarray
 
-# Remove any .nc files that already exist
-directory = next(os.walk("./"))
-for file_name in directory[-1]:
-    if file_name.endswith(".nc"):
-        os.remove(file_name)
 
-DATA_TIME = datetime.datetime(year=2022, month=1, day=7)
+DATA_TIME = datetime.datetime(year=2022, month=1, day=8)
 FILE_PATH = f"{DATA_TIME.strftime('%Y%m%d%H')}.nc"
 URL = (
         f"http://www.nohrsc.noaa.gov/snowfall_v2/data/"
         f"{DATA_TIME.strftime('%Y%m')}/sfav2_CONUS_24h_{DATA_TIME.strftime('%Y%m%d%H')}.nc"
     )
 
-urllib.request.urlretrieve(URL, FILE_PATH)
+if not os.path.exists(FILE_PATH):
+    # Remove any .nc files that already exist
+    directory = next(os.walk("./"))
+    for file_name in directory[-1]:
+        if file_name.endswith(".nc"):
+            os.remove(file_name)
 
-dataset = xarray.open_mfdataset(FILE_PATH)
+    urllib.request.urlretrieve(URL, FILE_PATH)
 
 
-def nohrsc_snow():
+def nohrsc_snow(extent):
+    min_lon, max_lon, min_lat, max_lat = extent
+
+    dataset = xarray.open_mfdataset(FILE_PATH)
+
+    mask_lon = (dataset.lon >= min_lon) & (dataset.lon <= max_lon)
+    mask_lat = (dataset.lat >= min_lat) & (dataset.lat <= max_lat)
+
+    dataset = dataset.where(mask_lon & mask_lat, drop=True)
     snow_data = dataset.Data * 39.3700787
 
     lats = snow_data.lat.values
