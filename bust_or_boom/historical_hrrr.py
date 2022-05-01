3e2507d4a6cb708aa72d65beb25a5b80f666b5f3
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-01 11:32:49 -0400

    plot historical HRRR

--- bust_or_boom/historical_hrrr.py
+++ bust_or_boom/historical_hrrr.py
@@ -3,6 +3,10 @@ import os
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import xarray
 
 # Remove any .grib2 files that already exist
@@ -12,7 +16,7 @@ for file_name in directory[-1]:
         os.remove(file_name)
 
 BUCKET_NAME = 'noaa-hrrr-bdp-pds'
-S3_OBJECT = 'hrrr.20220107/conus/hrrr.t00z.wrfsfcf01.grib2'
+S3_OBJECT = 'hrrr.20220107/conus/hrrr.t00z.wrfsfcf24.grib2'
 
 FILE_PATH = S3_OBJECT.split("/")[-1]
 
@@ -23,10 +27,32 @@ if not os.path.exists(FILE_PATH):
     with open(FILE_PATH, "wb") as file:
         file.write(obj)
 
+extent = (-79.05, -76.02, 37.585112, 39.6)
+
+fig: plt.Figure = plt.figure(figsize=(12, 6))
+ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
+ax.set_extent(extent)
+
+ax.add_feature(cfeature.LAND.with_scale("50m"))
+ax.add_feature(cfeature.OCEAN.with_scale("50m"))
+ax.add_feature(cfeature.STATES.with_scale("50m"))
 
 dataset = xarray.open_dataset(
     FILE_PATH,
     engine="cfgrib",
     filter_by_keys={'stepType': 'accum', 'typeOfLevel': 'surface'}
 )
+lons = dataset.lon.values
+lats = dataset.lat.values
 
+levels = [0.1, 1, 2, 3, 4, 6, 8, 12, 16, 20, 24, 30, 36, 48, 60, 72]
+cmap = colors.ListedColormap([
+    '#bdd7e7', '#6baed6', '#3182bd', '#08519c', '#082694', '#ffff96',
+    '#ffc400', '#ff8700', '#db1400', '#9e0000', '#690000', '#ccccff',
+    '#9f8cd8', '#7c52a5', '#561c72', '#40dfff'
+])
+norm = colors.BoundaryNorm(levels, cmap.N)
+
+ax.contourf(lons, lats, dataset["asnow"].values, levels, alpha=0.5, cmap=cmap, norm=norm, transform=ccrs.PlateCarree())
+plt.show()
