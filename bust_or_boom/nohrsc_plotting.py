210e709f756a5f51a2ec13d9174866790a022e36
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-02 17:12:59 -0400

    changes!

--- bust_or_boom/nohrsc_plotting.py
+++ bust_or_boom/nohrsc_plotting.py
@@ -5,7 +5,7 @@ import os
 import xarray
 
 
-DATA_TIME = datetime.datetime(year=2019, month=2, day=21)
+DATA_TIME = datetime.datetime(year=2021, month=2, day=13)
 FILE_PATH = f"{DATA_TIME.strftime('%Y%m%d%H')}.nc"
 URL = (
         f"http://www.nohrsc.noaa.gov/snowfall_v2/data/"
