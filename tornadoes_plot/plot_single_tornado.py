ff9e57d0059c75c5da1985ece4b0eab91c52be68
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-08 19:58:41 -0400

    add cartopy

--- tornadoes_plot/plot_single_tornado.py
+++ tornadoes_plot/plot_single_tornado.py
@@ -1,5 +1,20 @@
 from shapefile import Reader
 
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+
+extent = (-79.05, -76.02, 37.585112, 39.56)
+
+fig: plt.Figure = plt.figure(figsize=(12, 6))
+ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
+ax.set_extent(extent)
+
+ax.add_feature(cfeature.LAND.with_scale("50m"))
+ax.add_feature(cfeature.OCEAN.with_scale("50m"))
+ax.add_feature(cfeature.STATES.with_scale("50m"), lw=1.25)
+
 
 shp_pts = Reader("nws_dat_damage_pnts.shp")
 
