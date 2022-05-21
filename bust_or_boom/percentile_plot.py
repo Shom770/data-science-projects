8703ddbdf9043ce8f0cd6cf7f18a9050c815e3ab
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-21 01:19:30 -0400

    done

--- bust_or_boom/percentile_plot.py
+++ bust_or_boom/percentile_plot.py
@@ -28,7 +28,7 @@ def filter_datapoints(period, data):
 
 DIFF = 0.5
 SIGMA = 1
-MONTH = 1
+MONTH = None
 NUM_TO_MONTH = {
     1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
     7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
