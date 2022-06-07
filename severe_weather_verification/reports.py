c248b7626aa7058e0119d4eed04a1ebfd5176bb0
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-06 20:22:13 -0400

    read csv

--- severe_weather_verification/reports.py
+++ severe_weather_verification/reports.py
@@ -1,7 +1,14 @@
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import pandas as pd
 
 extent = (-79.9, -74.1, 37.1, 39.9)
+
+with open("220602_rpts_filtered.csv") as file:
+    all_text = file.read()
+    header_pos = [idx for idx, line in enumerate(all_text.split("\n")) if line.startswith("Time")]
+    reports_df = pd.read_csv(all_text, header=header_pos)
+    print(reports_df)
 
 fig: plt.Figure = plt.figure()
 ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
