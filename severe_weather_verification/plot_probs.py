b5c1135052c1c9db0d025498576c6c1690e069f7
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-07 19:59:01 -0400

    got the probs done, now time for the styling!

--- severe_weather_verification/plot_probs.py
+++ severe_weather_verification/plot_probs.py
@@ -30,7 +30,7 @@ def report_type_metadata(report_type):
 
 
 DIFF = 1
-REPORT_RADIUS = 5
+REPORT_TYPE = ReportType.WIND
 SIGMA = 0.75
 
 fig: plt.Figure = plt.figure(figsize=(12, 6))
@@ -38,16 +38,16 @@ ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 extent = (-79.9, -74.1, 37.1, 39.9)
 extent_lim = (extent[0] - DIFF, extent[1] + DIFF, extent[2] - DIFF, extent[3] + DIFF)
 
-ax.add_feature(cfeature.LAND.with_scale("10m"))
-ax.add_feature(cfeature.OCEAN.with_scale("10m"))
-ax.add_feature(cfeature.STATES.with_scale("10m"))
+ax.add_feature(cfeature.LAND.with_scale("10m"), zorder=100)
+ax.add_feature(cfeature.OCEAN.with_scale("10m"), zorder=200)
+ax.add_feature(cfeature.STATES.with_scale("10m"), zorder=300)
 
 
 ax.set_extent(extent)
 
-reports = all_reports(report_type=ReportType.WIND, extent=extent_lim)
-lons = np.arange(extent[0], extent[1] + 0.1, 0.1)
-lats = np.arange(extent[2], extent[3] + 0.1, 0.1)
+reports = all_reports(report_type=REPORT_TYPE, extent=extent_lim)
+lons = np.arange(extent[0], extent[1] + 0.2, 0.1)
+lats = np.arange(extent[2], extent[3] + 0.2, 0.1)
 z_data = []
 
 for idx, lat in enumerate(lats):
@@ -62,10 +62,14 @@ for idx, lat in enumerate(lats):
         z_data[-1].append((report_ct / 25) * 100)
 
 
+levels, cmap, norm = report_type_metadata(REPORT_TYPE)
+
 C = ax.contourf(
     *map(functools.partial(gaussian_filter, sigma=SIGMA), np.meshgrid(lons, lats)), gaussian_filter(z_data, SIGMA),
-    levels=[5, 15, 30, 45, 60]
+    levels=levels, cmap=cmap, norm=norm, transform=ccrs.PlateCarree(), zorder=150
 )
-fig.colorbar(C)
+
+CBAR = fig.colorbar(C, orientation="horizontal", ticks=levels[:-1], extend="max", shrink=0.75)
+CBAR.set_ticklabels(levels[:-1])
 
 plt.show()
