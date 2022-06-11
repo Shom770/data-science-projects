66a1c7b19ff9c514263ab2d50515b3b04edc3963
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-11 13:37:22 -0400

    subtract polygons

--- severe_weather_verification/outlooks.py
+++ severe_weather_verification/outlooks.py
@@ -22,7 +22,11 @@ def get_risks(date, report_type):
         for risk in risks["geometry"]["coordinates"]:
             all_polygons[-1].append(geometry.Polygon(risk[0]))
 
-    print(all_polygons)
+    for polygon_lst, next_pl in zip(all_polygons, all_polygons[1:]):
+        for idx, (p1, p2) in enumerate(zip(polygon_lst, next_pl)):
+            polygon_lst[idx] = p1 - p2
+
+
 
 
 get_risks(datetime.datetime(2022, 6, 2), ReportType.WIND)
