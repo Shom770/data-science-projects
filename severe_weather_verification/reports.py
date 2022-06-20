ab6cf6ebd0c8643091f3096334f8a576c486d1a9
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-19 22:24:17 -0400

    updates!

--- severe_weather_verification/reports.py
+++ severe_weather_verification/reports.py
@@ -14,24 +14,29 @@ SIG_COND_MAPPING = {ReportType.TORNADO: "F_Scale", ReportType.WIND: "Speed", Rep
 
 
 def all_reports(report_type, extent, day):
-    with open(f"{day.strftime('%y')[-2:] + day.strftime('%m%d')}_rpts_filtered.csv") as file:
-        all_text = file.read()
-        split_text = all_text.split("\n")
+    try:
+        with open(f"{day.strftime('%y')[-2:] + day.strftime('%m%d')}_rpts_filtered.csv") as file:
+            all_text = file.read()
+    except FileNotFoundError:
+        with open(f"{day.strftime('%y')[-2:] + day.strftime('%m%d')}_rpts.csv") as file:
+            all_text = file.read()
 
-        header_pos = [idx for idx, line in enumerate(split_text) if line.startswith("Time")]
+    split_text = all_text.split("\n")
 
-        if report_type == ReportType.TORNADO:
-            all_text = "\n".join(split_text[:header_pos[1]])
-        elif report_type == ReportType.WIND:
-            all_text = "\n".join(split_text[header_pos[1]:header_pos[2]])
-        elif report_type == ReportType.HAIL:
-            all_text = "\n".join(split_text[header_pos[2]:])
+    header_pos = [idx for idx, line in enumerate(split_text) if line.startswith("Time")]
 
-        reports_df = pd.read_csv(StringIO(all_text), on_bad_lines='skip')
-        condition = (
-                (extent[0] <= reports_df.Lon) & (reports_df.Lon <= extent[1])
-                & (extent[2] <= reports_df.Lat) & (reports_df.Lat <= extent[3])
-        )
-        reports_df = reports_df[condition]
+    if report_type == ReportType.TORNADO:
+        all_text = "\n".join(split_text[:header_pos[1]])
+    elif report_type == ReportType.WIND:
+        all_text = "\n".join(split_text[header_pos[1]:header_pos[2]])
+    elif report_type == ReportType.HAIL:
+        all_text = "\n".join(split_text[header_pos[2]:])
 
-        return [*zip(reports_df.Lon, reports_df.Lat, reports_df[SIG_COND_MAPPING[report_type]])]
+    reports_df = pd.read_csv(StringIO(all_text), on_bad_lines='skip')
+    condition = (
+            (extent[0] <= reports_df.Lon) & (reports_df.Lon <= extent[1])
+            & (extent[2] <= reports_df.Lat) & (reports_df.Lat <= extent[3])
+    )
+    reports_df = reports_df[condition]
+
+    return [*zip(reports_df.Lon, reports_df.Lat, reports_df[SIG_COND_MAPPING[report_type]])]
