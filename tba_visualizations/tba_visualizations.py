8250babc218fbe67b86e3681172c9c9e58f4a74f
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-26 17:47:27 -0400

    print out pretty printed dictionary

--- tba_visualizations/tba_visualizations.py
+++ tba_visualizations/tba_visualizations.py
@@ -1,3 +1,4 @@
+import json
 from os import environ
 
 import requests
@@ -10,4 +11,4 @@ URL = "https://www.thebluealliance.com/api/v3/team/frc4099/events/2022/statuses"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
 resp = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
-__import__("json").dumps(resp, indent=4)
+print(json.dumps(resp, indent=4))
