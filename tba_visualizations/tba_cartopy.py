793cd6a1745703b7dc44678f1aeac2a2604ffd64
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-29 23:36:38 -0400

    get coordinates by zipcode

--- tba_visualizations/tba_cartopy.py
+++ tba_visualizations/tba_cartopy.py
@@ -6,12 +6,16 @@ import numpy as np
 import requests
 from dotenv import load_dotenv
 from scipy import interpolate
+from uszipcode import SearchEngine
 
 load_dotenv()
 
 SESSION = requests.session()
-URL = "https://www.thebluealliance.com/api/v3/match/2022cmptx_f1m1/zebra_motionworks"
+URL = "https://www.thebluealliance.com/api/v3/teams/2022/8"
 TBA_API_KEY = environ["TBA_API_KEY"]
+SEARCH = SearchEngine()
 
 resp = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
-print(__import__("json").dumps(resp, indent=4))
+start = __import__("time").perf_counter()
+print(SEARCH.by_zipcode(int(resp[-1]["postal_code"])))
+print(__import__("time").perf_counter() - start)
