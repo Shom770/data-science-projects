944ee413d12ddcb568a92b1f811786baf04f97ea
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-30 01:09:11 -0400

    Write data to another file -- efficiency

---
+++ tba_visualizations/generate_data.py
@@ -0,0 +1,33 @@
+import json
+import logging
+
+import geopy.distance
+import numpy as np
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+extent = (-126.298828, -66.313477, 24.686952, 49.439557)
+
+lats = np.arange(extent[2], extent[3], 1.25)
+lons = np.arange(extent[0], extent[1], 1.25)
+
+with open("all_teams.json") as file:
+    all_teams = json.load(file)
+
+team_loc = list(all_teams.values())
+z_data = []
+
+for idx, lat in enumerate(lats):
+    filtered_locations = [loc for loc in team_loc if geopy.distance.distance(loc[::-1], (lat, loc[0])).miles <= 100]
+    z_data.append([])
+
+    for lon in lons:
+        z_data[-1].append(
+            len([loc for loc in filtered_locations if geopy.distance.distance(loc[::-1], (lat, lon)).miles <= 100])
+        )
+    logger.info(f"{idx + 1}/{len(lats)} latitudes finished calculating.")
+
+with open("num_teams_data.json", "w") as file:
+    json.dump(z_data, file, indent=4)
--- tba_visualizations/tba_cartopy.py
+++ tba_visualizations/tba_cartopy.py
@@ -2,20 +2,17 @@ import json
 
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
+import geopy.distance
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 extent = (-126.298828, -66.313477, 24.686952, 49.439557)
 
 lats = np.arange(extent[2], extent[3], 1.25)
 lons = np.arange(extent[0], extent[1], 1.25)
 
-with open("all_teams.json") as file:
-    all_teams = json.load(file)
-
-z_data = []
-
+with open("num_teams_data.json") as file:
+    z_data = json.load(file)
 
 fig: plt.Figure = plt.figure(figsize=(12, 6))
 ax: plt.Axes = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
@@ -26,5 +23,10 @@ ax.add_feature(cfeature.LAND.with_scale("110m"))
 ax.add_feature(cfeature.LAKES.with_scale("110m"))
 ax.add_feature(cfeature.OCEAN.with_scale("110m"), zorder=100)
 ax.add_feature(cfeature.STATES.with_scale("110m"), zorder=200)
+
+C = ax.contourf(
+    lons, lats, z_data, zorder=50, antialiased=True
+)
+fig.colorbar(C)
 
 plt.show()
