c75d35d454cc6d462d7d0ea799d0e81da55b21db
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-06 00:23:30 -0400

    Quick scrip tfinsihed

--- tba_visualizations/tba_quick_script.py
+++ tba_visualizations/tba_quick_script.py
@@ -11,21 +11,39 @@ URL = "https://www.thebluealliance.com/api/v3/event/2022hop/matches"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
 all_matches = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+scouting_matches = []
+match_alliances = []
 
 with open("quick_script_data.txt") as file:
     teams = {"frc" + line.strip("|").split("|")[0] for idx, line in enumerate(file.readlines()) if idx != 0}
 
-
 for match in all_matches:
-    resp = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+    if any(
+            team in teams for team in match["alliances"]["blue"]["team_keys"] + match["alliances"]["red"]["team_keys"]
+    ):
+        scouting_matches.append(
+            {
+                "Match Number": match["match_number"],
+                "Video URL": f"https://youtube.com/watch?v={match['videos'][-1]['key']}"
+            }
+        )
+        match_alliances.append(
+            (
+                {"Match Key": match["key"]}
+                | {
+                    alliance_pos: team.removeprefix("frc")
+                    for alliance_pos, team in zip(
+                        ["Red 1", "Red 2", "Red 3"], match["alliances"]["red"]["team_keys"]
+                    )
+                }
+                | {
+                    alliance_pos: team.removeprefix("frc")
+                    for alliance_pos, team in zip(
+                        ["Blue 1", "Blue 2", "Blue 3"], match["alliances"]["blue"]["team_keys"]
+                    )
+                }
+            )
+        )
 
-    resp = SESSION.get(URL.format(match_key=event_key), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
-    all_event_ct[event_resp["name"]] = len([team for team in resp if team.removeprefix("frc") in teams])
-
-print("\n".join(
-    [
-        f"{key} - {value} teams going to IRI"
-        for key, value in sorted(all_event_ct.items(), key=lambda tup: tup[1], reverse=True)
-    ]
-))
-# print(max(all_event_ct.items(), key=lambda tup: tup[1])[0])
+print(pd.DataFrame(scouting_matches).to_csv())
+print(pd.DataFrame(match_alliances).to_csv())
