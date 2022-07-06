fcfb409d38c29455340f938da03e9fc4c7193375
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-05 20:42:07 -0400

    finished script

--- tba_visualizations/tba_quick_script.py
+++ tba_visualizations/tba_quick_script.py
@@ -1,14 +1,27 @@
-import datetime
 from os import environ
 
-import matplotlib.pyplot as plt
-import numpy as np
 import requests
 from dotenv import load_dotenv
-from scipy import interpolate
 
 load_dotenv()
 
 SESSION = requests.session()
-URL = "https://www.thebluealliance.com/api/v3/team/frc4099/matches/{year}/simple"
+URL = "https://www.thebluealliance.com/api/v3/event/{match_key}/teams/keys"
+EVENT_URL = "https://www.thebluealliance.com/api/v3/event/{match_key}/simple"
 TBA_API_KEY = environ["TBA_API_KEY"]
+
+events = ["2022carv", "2022gal", "2022hop", "2022new", "2022roe", "2022tur"]
+event_names = []
+
+with open("quick_script_data.txt") as file:
+    teams = {line.strip("|").split("|")[0] for idx, line in enumerate(file.readlines()) if idx != 0}
+
+all_event_ct = {}
+for event_key in events:
+    event_resp = SESSION.get(EVENT_URL.format(match_key=event_key), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+
+    resp = SESSION.get(URL.format(match_key=event_key), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+    all_event_ct[event_resp["name"]] = len([team for team in resp if team.removeprefix("frc") in teams])
+
+print("\n".join([f"{key} - {value} teams going to IRI" for key, value in all_event_ct.items()]))
+# print(max(all_event_ct.items(), key=lambda tup: tup[1])[0])
