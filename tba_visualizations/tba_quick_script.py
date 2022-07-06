f902d540e84960f778c10aabc607a1bb316436d5
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-05 23:49:34 -0400

    redoing script -- basic structure

--- tba_visualizations/tba_quick_script.py
+++ tba_visualizations/tba_quick_script.py
@@ -1,24 +1,23 @@
 from os import environ
 
+import pandas as pd
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SESSION = requests.session()
-URL = "https://www.thebluealliance.com/api/v3/event/{match_key}/teams/keys"
-EVENT_URL = "https://www.thebluealliance.com/api/v3/event/{match_key}/simple"
+URL = "https://www.thebluealliance.com/api/v3/event/2022hop/matches"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
-events = ["2022carv", "2022gal", "2022hop", "2022new", "2022roe", "2022tur"]
-event_names = []
+all_matches = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
 
 with open("quick_script_data.txt") as file:
-    teams = {line.strip("|").split("|")[0] for idx, line in enumerate(file.readlines()) if idx != 0}
+    teams = {"frc" + line.strip("|").split("|")[0] for idx, line in enumerate(file.readlines()) if idx != 0}
 
-all_event_ct = {}
-for event_key in events:
-    event_resp = SESSION.get(EVENT_URL.format(match_key=event_key), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+
+for match in all_matches:
+    resp = SESSION.get(URL, headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
 
     resp = SESSION.get(URL.format(match_key=event_key), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
     all_event_ct[event_resp["name"]] = len([team for team in resp if team.removeprefix("frc") in teams])
