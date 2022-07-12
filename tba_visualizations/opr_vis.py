686c465b6c0a86333c2375e55f43cddfcf79851e
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-12 16:54:46 -0400

    best to worst oprs

--- tba_visualizations/opr_vis.py
+++ tba_visualizations/opr_vis.py
@@ -9,4 +9,12 @@ SESSION = requests.session()
 URL = "https://www.thebluealliance.com/api/v3/event/2022chcmp/oprs"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
-print(SESSION.get(URL, headers={"X-TBA-Auth-key": TBA_API_KEY}).json()["oprs"])
+print(
+    dict(
+        sorted(
+            SESSION.get(URL, headers={"X-TBA-Auth-key": TBA_API_KEY}).json()["oprs"].items(),
+            key=lambda tup: tup[1],
+            reverse=True
+        )
+    )
+)
