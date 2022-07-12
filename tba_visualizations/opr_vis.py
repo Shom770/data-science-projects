ad3193247456849dff774d5c45bd4326afb47600
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-12 17:09:03 -0400

    wip

--- tba_visualizations/opr_vis.py
+++ tba_visualizations/opr_vis.py
@@ -1,20 +1,33 @@
 from os import environ
 
+import numpy as np
 import requests
 from dotenv import load_dotenv
+from scipy.stats import skew
 
 load_dotenv()
 
 SESSION = requests.session()
-URL = "https://www.thebluealliance.com/api/v3/event/2022chcmp/oprs"
+URL = "https://www.thebluealliance.com/api/v3/event/{key}/oprs"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
+ccvm_dataset = np.array(
+    list(
+        dict(
+            sorted(
+                SESSION.get(
+                    URL.format(key="2022tur"),
+                    headers={"X-TBA-Auth-key": TBA_API_KEY}
+                ).json()["ccwms"].items(),
+                key=lambda tup: tup[1],
+                reverse=True
+            )
+        ).values()
+    )
+)
+
 print(
-    dict(
-        sorted(
-            SESSION.get(URL, headers={"X-TBA-Auth-key": TBA_API_KEY}).json()["oprs"].items(),
-            key=lambda tup: tup[1],
-            reverse=True
-        )
+    skew(
+        ccvm_dataset
     )
 )
