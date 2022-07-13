cc1b62f8b1a669b51eb480987de5b0ac0ef9a341
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-12 20:21:15 -0400

    create dataset of all poss alliances

--- tba_visualizations/opr_vis.py
+++ tba_visualizations/opr_vis.py
@@ -1,3 +1,4 @@
+import itertools
 from math import asinh
 from os import environ
 
@@ -12,24 +13,13 @@ SESSION = requests.session()
 URL = "https://www.thebluealliance.com/api/v3/event/{key}/oprs"
 TBA_API_KEY = environ["TBA_API_KEY"]
 
+all_ccwms = SESSION.get(URL.format(key="2022chcmp"), headers={"X-TBA-Auth-key": TBA_API_KEY}).json()["ccwms"]
+
 ccwm_dataset = np.array(
-    list(
-        dict(
-            sorted(
-                SESSION.get(
-                    URL.format(key="2022chcmp"),
-                    headers={"X-TBA-Auth-key": TBA_API_KEY}
-                ).json()["ccwms"].items(),
-                key=lambda tup: tup[1],
-                reverse=True
-            )
-        ).values()
-    )
+    sorted({sum(alliance_sum) for alliance_sum in itertools.product(all_ccwms.values(), repeat=3)})
 )
 
 ccwm_dataset = min(
     [np.array([asinh(value / k_factor) for value in ccwm_dataset]) for k_factor in range(1, 21, 5)],
     key=lambda arr: abs(skew(arr))
 )
-
-print(ccwm_dataset.mean())
