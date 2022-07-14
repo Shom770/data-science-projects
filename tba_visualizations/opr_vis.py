4d595cd3e489da09e5fba01240b5ffabc51eefd3
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-14 08:44:02 -0400

    fixing errors when renaming

--- tba_visualizations/opr_vis.py
+++ tba_visualizations/opr_vis.py
@@ -17,21 +17,22 @@ EVENT_KEY = "2022tur"
 all_ccwms = SESSION.get(
     URL.format(key=EVENT_KEY, mode="oprs"), headers={"X-TBA-Auth-key": TBA_API_KEY}
 ).json()["ccwms"]
+all_alliances = SESSION.get(
+     URL.format(key=EVENT_KEY, mode="alliances"), headers={"X-TBA-Auth-key": TBA_API_KEY}
+).json()
 
-all_alliances = [
-    [all_ccwms[pick] for pick in alliance["picks"]]for alliance in SESSION.get(
-        URL.format(key=EVENT_KEY, mode="alliances"), headers={"X-TBA-Auth-key": TBA_API_KEY}
-    ).json()
+alliance_sums = [
+    [all_ccwms[pick] for pick in alliance["picks"]] for alliance in all_alliances
 ]
 
 ccwm_dataset = np.array(
     sorted({
         sum(alliance_sum)
-        for alliance_sum in itertools.product(itertools.chain.from_iterable(all_alliances), repeat=3)
+        for alliance_sum in itertools.product(itertools.chain.from_iterable(alliance_sums), repeat=3)
         if len(alliance_sum) == 3
     })
 )
-all_alliances = list(map(sum, all_alliances))
+alliance_sums = list(map(sum, alliance_sums))
 
 ccwm_dataset, k_factor = min(
     [(np.array([asinh(value / k_factor) for value in ccwm_dataset]), k_factor) for k_factor in range(1, 102, 10)],
@@ -40,6 +41,8 @@ ccwm_dataset, k_factor = min(
 
 percentiles_picks = []
 
-for ccwm_sum in all_alliances:
+for ccwm_sum in alliance_sums:
     z_score = (asinh(ccwm_sum / k_factor) - np.mean(ccwm_dataset)) / np.std(ccwm_dataset)
     percentiles_picks.append((1 - norm.sf(z_score)) * 100)
+
+print(percentiles_picks)
