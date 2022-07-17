92e00a2cc2bb3e7966e3c5f337f8ce741fe849b2
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-17 00:12:11 -0400

    change to OPR - more accurate

--- tba_visualizations/opr_vis.py
+++ tba_visualizations/opr_vis.py
@@ -16,7 +16,7 @@ EVENT_KEY = "2022tur"
 
 all_ccwms = SESSION.get(
     URL.format(key=EVENT_KEY, mode="oprs"), headers={"X-TBA-Auth-key": TBA_API_KEY}
-).json()["ccwms"]
+).json()["oprs"]
 all_alliances = SESSION.get(
      URL.format(key=EVENT_KEY, mode="alliances"), headers={"X-TBA-Auth-key": TBA_API_KEY}
 ).json()
@@ -25,24 +25,28 @@ alliance_sums = [
     [all_ccwms[pick] for pick in alliance["picks"]] for alliance in all_alliances
 ]
 
-ccwm_dataset = np.array(
+opr_dataset = np.array(
     sorted({
-        sum(alliance_sum)
-        for alliance_sum in itertools.product(itertools.chain.from_iterable(alliance_sums), repeat=3)
-        if len(alliance_sum) == 3
+        sum(alliance_sum) / len(alliance_sum)
+        for alliance_sum in itertools.product(itertools.chain.from_iterable(alliance_sums), repeat=len(alliance_sums[0]))
+        if len(alliance_sum) == len(alliance_sums[0])
     })
 )
-alliance_sums = list(map(sum, alliance_sums))
+alliance_sums = [sum(all_sum) / len(all_sum) for all_sum in alliance_sums]
 
-ccwm_dataset, k_factor = min(
-    [(np.array([asinh(value / k_factor) for value in ccwm_dataset]), k_factor) for k_factor in range(1, 102, 10)],
-    key=lambda arr: abs(skew(arr[0]))
+mode, opr_dataset = min(
+    (
+        (np.log, np.log(opr_dataset)),
+        (np.reciprocal, 1 / opr_dataset),
+        (np.sqrt, opr_dataset ** 0.5)
+    ),
+    key=lambda arr: abs(skew(arr[1]))
 )
 
 percentiles_picks = []
 
-for ccwm_sum in alliance_sums:
-    z_score = (asinh(ccwm_sum / k_factor) - np.mean(ccwm_dataset)) / np.std(ccwm_dataset)
+for opr_sum in alliance_sums:
+    z_score = (mode(opr_sum) - opr_dataset.mean()) / opr_dataset.std()
     percentiles_picks.append((1 - norm.sf(z_score)) * 100)
 
-print(percentiles_picks)
+print(percentiles_picks, [alliance["picks"] for alliance in all_alliances])
