41ae0f2eb56a23907af749b0338eb5482402ea51
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-19 23:13:45 -0400

    amount of teams that go to worlds

--- tba_visualizations/tba_rookie_year.py
+++ tba_visualizations/tba_rookie_year.py
@@ -6,33 +6,38 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SESSION = requests.session()
-URL = "https://www.thebluealliance.com/api/v3/teams/2022/{page}"
+URL = "https://www.thebluealliance.com/api/v3/district/{key}/teams/keys"
 TBA_API_KEY = environ["TBA_API_KEY"]
+EVENT_KEYS = [
+    "2022carv",
+    "2022gal",
+    "2022hop",
+    "2022new",
+    "2022roe",
+    "2022tur"
+]
 
-chs_teams = set(SESSION.get(
-    "https://www.thebluealliance.com/api/v3/district/2022chs/teams/keys",
+districts = SESSION.get(
+    "https://www.thebluealliance.com/api/v3/districts/2022",
     headers={"X-TBA-Auth-Key": TBA_API_KEY}
-).json())
-chs_rookie_years = []
-california_rookie_years = []
+).json()
+worlds_teams = []
+
+for event_key in EVENT_KEYS:
+    worlds_teams.extend(SESSION.get(
+        f"https://www.thebluealliance.com/api/v3/event/{event_key}/teams/keys",
+        headers={"X-TBA-Auth-Key": TBA_API_KEY}
+    ).json())
+
+worlds_teams = set(worlds_teams)
+
+all_teams = {}
+
+for district in districts:
+    teams = SESSION.get(URL.format(key=district["key"]), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
+    district_teams_in_worlds = len(worlds_teams.intersection(teams)) / len(teams)
+    all_teams[district['display_name']] = district_teams_in_worlds
 
 
-for page_num in range(0, 20):
-    resp = SESSION.get(URL.format(page=page_num), headers={"X-TBA-Auth-Key": TBA_API_KEY}).json()
-    if not resp:
-        break
-
-    for team in resp:
-        if team["state_prov"] == "California":
-            california_rookie_years.append(team["rookie_year"])
-        elif team["key"] in chs_teams:
-            chs_rookie_years.append(team["rookie_year"])
-
-
-print(
-    f"Average rookie year of CHS teams in 2022 - {round(sum(chs_rookie_years) / len(chs_rookie_years))}"
-)
-print(
-    f"Average rookie year of Californian teams in 2022 - "
-    f"{round(sum(california_rookie_years) / len(california_rookie_years))}"
-)
+for district_name, world_per in dict(sorted(all_teams.items(), key=lambda tup: tup[1], reverse=True)).items():
+    print(f"{district_name} has {world_per * 100:.1f}% of their teams go to Worlds")
