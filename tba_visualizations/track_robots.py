21df0eabf6cfa14b9da847ada1d2734a67a735a1
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-07-30 00:07:06 -0400

    failure but close enough

--- tba_visualizations/track_robots.py
+++ tba_visualizations/track_robots.py
@@ -1,11 +1,88 @@
 from os import environ
+from threading import Thread
 
 import cv2
+import cv2.legacy
+import matplotlib.pyplot as plt
+import yt_dlp
 from dotenv import load_dotenv
 from tbapy import TBA
 
 load_dotenv()
 
+WIDTH = 1920
+HEIGHT = 1080
+tba = TBA(environ["TBA_API_KEY"])
+
 
 def track_robots(match_key: str) -> None:
-    ...
+    video_url = f"https://youtube.com/watch?v={tba.match(key=match_key)['videos'][0]['key']}"
+
+    with yt_dlp.YoutubeDL({}) as ydl:
+        ydl.download([video_url])
+
+    vs = cv2.VideoCapture("Einstein Final 1 - 2022 FIRST Championship [EgPMnpp-PnQ].mp4")
+
+    tracker = cv2.legacy.TrackerCSRT_create()
+
+    initBB = None
+
+    positions_x = []
+    positions_y = []
+
+    figure, ax = plt.subplots(figsize=(12, 6))
+
+    ax.set_xlim([0, 1920])
+    ax.set_ylim([0, 1080])
+
+    grabbed = True
+    initialized = False
+
+    while grabbed:
+        grabbed, frame = vs.read()
+
+        if initBB is not None:
+            # grab the new bounding box coordinates of the object
+            (success, box) = tracker.update(frame)
+            # check to see if the tracking was a success
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                positions_x.append(x + w / 2)
+                positions_y.append(y + h / 2)
+                cv2.rectangle(
+                    frame, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2
+                )
+            else:
+                print(success)
+
+        key = cv2.waitKey(1) & 0xFF
+        cv2.imshow("Frame", frame)
+
+        # if the 's' key is selected, we are going to "select" a bounding
+        # box to track
+        if key == ord("s"):
+            # select the bounding box of the object we want to track (make
+            # sure you press ENTER or SPACE after selecting the ROI)
+            initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+                                   showCrosshair=True)
+            # start OpenCV object tracker using the supplied bounding box
+            # coordinates, then start the FPS throughput estimator as well
+            if not initialized:
+                tracker.init(frame, initBB)
+                initialized = True
+            else:
+                tracker = cv2.legacy.TrackerCSRT_create()
+                tracker.init(frame, initBB)
+
+        elif key == ord("q"):
+            break
+
+    ax.plot(positions_x, positions_y)
+    plt.show()
+
+    vs.release()
+    cv2.destroyAllWindows()
+
+
+track_robots(match_key="2022cmptx_f1m1")
